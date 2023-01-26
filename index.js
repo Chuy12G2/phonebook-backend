03dcac087ad3066229b729be45d50ae0904b4331
Jesus <chuygs12@gmail.com>
2023-01-26 13:15:10 -0600

    add eslint configuration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -7,17 +8,17 @@ const { Person } = require('./models/person')
 
 //const morgan = require('morgan')
 
-const url = process.env.MONGODB_URI  
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to mongoDB (index)')
-    })
-    .catch(error => {
-        console.log('error connecting to mongoDB', error.message)
-    })
+  .then(() => {
+    console.log('connected to mongoDB (index)')
+  })
+  .catch(error => {
+    console.log('error connecting to mongoDB', error.message)
+  })
 
 
 //app.use(morgan('tiny'))
@@ -26,65 +27,62 @@ app.use(cors())
 app.use(express.static('build'))
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.name)
-    if(error.name === 'CastError'){
-        return res.status(404).send({ error: 'malformed id'})
-    }else if(error.name === 'ValidationError'){
-        return res.status(400).json({error: error.message})
-    }
-    next(error)
+  console.error(error.name)
+  if(error.name === 'CastError'){
+    return res.status(404).send({ error: 'malformed id' })
+  }else if(error.name === 'ValidationError'){
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
-    }) 
-})
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })})
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
+  res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    Person.findById(id).then(person => {
-        if(person){
-            res.json(person)
-        }else{
-            res.status(404).end()
-        }
-    })
+  const id = req.params.id
+  Person.findById(id).then(person => {
+    if(person){
+      res.json(person)
+    }else{
+      res.status(404).end()
+    }})
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findByIdAndRemove(id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (req, res) => {
-    
-    const body = req.body
-    
-    const newPerson = new Person({
-        name: body.name,
-        phone: body.phone,
-    })
 
-    newPerson.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+  const body = req.body
+  const newPerson = new Person({
+    name: body.name,
+    phone: body.phone,
+  })
+
+  newPerson.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 
 })
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,39 +1,41 @@
+/* eslint-disable no-undef */
 require('dotenv').config()
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('CONNECTING TO', url)
+console.log('CONNECTING TO¡, url')
 
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to mongoDB (models)')
-    })
-    .catch(error => {
-        console.log('error connecting to mongoDB (models)', error.message)
-    })
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connected to mongoDB (models)')
+  })
+  .catch((error) => {
+    console.log('error connecting to mongoDB (models)', error.message)
+  })
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
-    },
-    phone:{
-        type: String,
-        minLength: 3,
-        required: true,
-    }, 
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  phone: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports.Person = mongoose.model('Person', personSchema)
